b03dce42eca86409084c53ce9f69c432c4f39017
Jain <vaibhjain@paypalcorp.com>
2017-06-20 15:59:26 +0530

    Improving dayofweek program

--- EverGreenJavaPrograms/src/uniqueCodes/DayOfWeek.java
+++ EverGreenJavaPrograms/src/uniqueCodes/DayOfWeek.java
@@ -3,13 +3,18 @@ package uniqueCodes;
 public class DayOfWeek {
 
 	static int dow(int y, int m, int d) {
-	    int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
-	    if(m<3)
-	    	y--;
-	    return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+		int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+		if (m < 3)
+			y--;
+		return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
 	}
-	
+
+	enum WeekDay {
+		SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(dow(1991, 05, 20));
+		WeekDay weekday = WeekDay.values()[dow(1991, 05, 20)];
+		System.out.println(weekday);
 	}
 }
