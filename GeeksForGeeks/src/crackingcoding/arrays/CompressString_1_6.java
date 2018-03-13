db199417fd7f0ca5e7231cbb1a788cdf05426e83
Vaibhav Jain <vaibhavjain_1@yahoo.com>
2018-03-13 14:41:36 +0530

    Adding Compress String solution

--- GeeksForGeeks/src/crackingcoding/arrays/CompressString_1_6.java
+++ GeeksForGeeks/src/crackingcoding/arrays/CompressString_1_6.java
@@ -10,11 +10,40 @@ public class CompressString_1_6 {
 
 	public static void main(String[] args) {
 		String tobeCompressed1 = "aabcccccaaa";
-		String tobeCompressed2 = "aabcccccaaa";
-		
+		String tobeCompressed2 = "abcccccaaadee";
+		String tobeCompressed3 = "abcccccaaade";
+		System.out.println(getCompressedString(tobeCompressed1));
+		System.out.println(getCompressedString(tobeCompressed2));
+		System.out.println(getCompressedString(tobeCompressed3));
 	}
     
-	public static String getCompressedString(){
-		return null;
+	public static String getCompressedString(String tempi){
+		char[] toCompress = tempi.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		char currChar = toCompress[0];
+		int currCount = 1;
+		for (int i = 1; i < toCompress.length; i++) {
+			if(i==toCompress.length-1){
+				if(toCompress[i]==toCompress[i-1]){
+					sb.append(currChar+""+(currCount+1));
+				}
+				else{
+					sb.append(currChar+""+currCount);
+					sb.append(toCompress[i]+""+1);
+				}
+			}
+			else if(currChar==toCompress[i]){
+				currCount++;
+			}
+			else{
+				sb.append(currChar+""+currCount);
+				currChar = toCompress[i];
+				currCount = 1;
+			}
+		}
+		if(tempi.length()<=sb.length())
+			return tempi;
+		else
+			return sb.toString();
 	}
 }
