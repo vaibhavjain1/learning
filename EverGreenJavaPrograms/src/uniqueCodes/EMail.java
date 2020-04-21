a1d608cc0f0cd82692c75eefd3adfb4cb11fa8a7
vaibhavjain2 <vaibhavjain_1@yahoo.com>
2020-04-22 00:31:30 +0530

    Adding program for email.

--- EverGreenJavaPrograms/src/uniqueCodes/EMail.java
+++ EverGreenJavaPrograms/src/uniqueCodes/EMail.java
@@ -1,10 +1,84 @@
 package uniqueCodes;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class EMail {
-	
-	//https://myaccount.google.com/lesssecureapps
+
 	public static void main(String[] args) {
-		
+		// Sender's email ID needs to be mentioned
+		String username = "abvaibhav@gmail.com";
+		String password = "";
+
+		// Recipient's email ID needs to be mentioned.
+		String to = "abvaibhav@gmail.com";
+
+		Properties prop = setupGMailServerSSL();
+
+		// Get the default Session object.
+		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		session.setDebug(true);
+		try {
+			// Create a default MimeMessage object.
+			MimeMessage message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(username));
+
+			// Set To: header field of the header.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			// Set Subject: header field
+			message.setSubject("This is the Subject Line!");
+
+			// Now set the actual message
+			message.setText("This is actual message");
+
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+
+	//https://myaccount.google.com/lesssecureapps
+	
+	private static Properties setupGMailServerSSL() {
+		// Get system properties
+		Properties prop = System.getProperties();
+
+		// Setup mail server
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		return prop;
+	}
+
+	private static Properties setupGMailServerTLS() {
+		// Get system properties
+		Properties prop = System.getProperties();
+
+		// Setup mail server
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true");
+		return prop;
 	}
 
 }
