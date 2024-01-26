22fbf4238fb7b324258088037cbdc737c22ed485
vaibhavjain1 <vaibhavjain_1@yahoo.com>
2024-01-26 21:52:40 +0530

    updating spring boot

--- SpringFramework/SpringBOOT/src/main/java/com/springBoot/Application.java
+++ SpringFramework/SpringBOOT/src/main/java/com/springBoot/Application.java
@@ -1,13 +1,11 @@
-package com.springBoot.SpringBOOT;
+package com.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
--- SpringFramework/SpringBOOT/src/test/java/com/springBoot/ApplicationTests.java
+++ SpringFramework/SpringBOOT/src/test/java/com/springBoot/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springBoot.SpringBOOT;
+package com.springBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
