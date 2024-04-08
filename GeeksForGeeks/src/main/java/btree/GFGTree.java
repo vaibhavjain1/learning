89668aadf64f6a1b354190a2dbc627c83ac4bdae
Vaibhav Jain <vaibhavjain_1@yahoo.com>
2024-04-08 15:08:28 +0530

    Adding GFG Tree

---
+++ GeeksForGeeks/src/main/java/btree/GFGTree.java
@@ -0,0 +1,70 @@
+package btree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GFGTree {
+
+    static Node buildTree(String str){
+
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        String preOrder = "14 14 3 N 8 8 12 N 6 17 3 N 1 11 10 N 6 6 13 N 10 17 7 N 11 7";
+        Node root = buildTree(preOrder);
+        Node.printTreeDiagram(root);
+    }
+}
